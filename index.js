78167e2180b13e7d54a5eb7aa266b4fe09741c97
Nahid30 <nahidshuvo30@gmail.com>
2022-07-14 19:51:12 +0600

    create DELETE api for delete a perticular student through his _id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -35,6 +35,17 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
                 const result = await studentCollection.insertOne(addNewStudent);
                 res.send(result);
             }) 
+
+            // DELETE 
+            app.delete('/student/:id', async (req, res) => {
+                const id = req.params.id;
+                const query = { _id: ObjectId(id) };
+                const resultDelete = await studentCollection.deleteOne(query);
+                res.send(resultDelete);
+    
+            })
+
+            
 
 
         }
